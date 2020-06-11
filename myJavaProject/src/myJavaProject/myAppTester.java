cb1aa8e97b0b64f579c2f682d088cf3d22d362a6
lucifer <dubeys@student.douglascollege.ca>
2020-06-11 03:52:09 -0700

    This is a committ

--- myJavaProject/src/myJavaProject/myAppTester.java
+++ myJavaProject/src/myJavaProject/myAppTester.java
@@ -1,11 +1,27 @@
 package myJavaProject;
+import java.util.*;
 
 public class myAppTester {
+	
+	static Scanner sc= new Scanner(System.in);
 	
 	public static void main(String args[])
 	{
 		System.out.println("Hello World");
 		System.out.println("On the new branch b1");
+		String message =  Message();
+		System.out.print("Your Message is : "+ message);
+		
+		
+		
 	}
 
+	public static String Message()
+	{
+		System.out.println("Enter a message Please");
+		String msg= sc.nextLine();
+		
+		return msg;
+		
+	}
 }
